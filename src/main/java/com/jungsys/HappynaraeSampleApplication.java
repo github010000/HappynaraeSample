4b9ba43ef7c0738c21904f231a2fdf4c734cd6b1
김영만 <1800534@partner.skcc.com>
2019-02-20 13:47:49 +0900

    Initial commit

---
+++ src/main/java/com/jungsys/HappynaraeSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jungsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappynaraeSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappynaraeSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jungsys/HappynaraeSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jungsys;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HappynaraeSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
