707c8e1862974d298d5445aad76666ae2595b5e3
unknown <Administrator@SKCC14D00789.SKCC.NET>
2019-04-10 15:02:52 +0900

    G2 Total Test Init

--- src/main/java/com/jungsys/controller/RootController.java
+++ src/main/java/com/jungsys/controller/RootController.java
@@ -1,6 +1,7 @@
 package com.jungsys.controller;
 
-import com.jungsys.utils.Loggie;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,11 @@ import java.util.Map;
 
 @RestController
 public class RootController {
+	private final static Logger logger = LoggerFactory.getLogger(RootController.class);
+	
     @RequestMapping("/")
     public String root() {
-        Loggie.info("##### Root path requested");
+        logger.info("##### Root path requested");
 
         return new String("You have visited Happynarae Sample.");
     }
@@ -20,7 +23,7 @@ public class RootController {
     @RequestMapping("/sleep")
     public Map<String, Object> apps(@RequestParam Map<String, String> param) {
         String millis = param.get("millis");
-        Loggie.info("##### sleep entered...millis=" + millis);
+        logger.info("##### sleep entered...millis=" + millis);
 
         Map<String, Object> myResult = new HashMap<String, Object>();
         myResult.put("svc_name", "SLEEP");
@@ -31,12 +34,12 @@ public class RootController {
             Thread.sleep(Long.parseLong(millis));
         }
         catch(Exception e) {
-            Loggie.error(e);
+            logger.error(e.toString());
             myResult.put("result", "-1");
             myResult.put("reason", "Exception");
         }
 
-        Loggie.info("##### the end..." + myResult.toString());
+        logger.info("##### the end..." + myResult.toString());
         return myResult;
     }
 }
